07469dad662115fedf75eb57ecc85cb5101277ef
deepa2307 <barmeda2307@gmail.com>
2024-02-01 23:00:04 +0530

    added Expense

---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,13 @@
+function ExpenseItems() {
+  return (
+    <div>
+      <h2>Expense Items </h2>
+      <h4>Food Rs 10</h4>
+      <h4>Oetrol Rs 100</h4>
+      <h4>Bike Rs 50</h4>
+      <h4>Movies Rs 400</h4>
+    </div>
+  );
+}
+
+export default ExpenseItems;
