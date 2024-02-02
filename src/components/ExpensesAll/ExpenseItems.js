048cb1a9919e6c86d3d5ef0eedf5bd74df76a67f
deepa2307 <barmeda2307@gmail.com>
2024-02-02 21:37:28 +0530

    added a button to update the amount of Expense

--- src/components/ExpensesAll/ExpenseItems.js
+++ src/components/ExpensesAll/ExpenseItems.js
@@ -2,18 +2,26 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import ExpenseDetails from "./ExpenseDetalis";
 import "./ExpenseItem.css";
+import React, { useState } from "react";
 
-function ExpenseItems(props) {
+const ExpenseItems = (props) => {
+  const [amount, setAmount] = useState(props.amount);
+
+  const clickHandle = () => {
+    setAmount("100");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <ExpenseDetails
         title={props.title}
         location={props.location}
-        amount={props.amount}
+        amount={amount}
       />
+      <button onClick={clickHandle}> $100 </button>
     </Card>
   );
-}
+};
 
 export default ExpenseItems;
