dd1d83c35378f86f51417c07f4702bf2e950fdff
deepa2307 <barmeda2307@gmail.com>
2024-02-05 12:29:38 +0530

    updating the expense in the screen

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
+import { useState } from "react";
 import Expenses from "./components/ExpensesAll/Expenses";
 import NewExpense from "./components/newExpenseInput/NewExp";
 
+const InitialExpense = [
+  {
+    id: "e1",
+    title: " Car",
+    amount: "2500",
+    date: new Date(2021, 2, 28),
+    location: "baroda",
+  },
+  {
+    id: "e2",
+    title: "Mobile",
+    amount: 230,
+    date: new Date(2021, 2, 28),
+    location: "surat",
+  },
+  {
+    id: "e3",
+    title: "house EMI",
+    amount: 2500,
+    date: new Date(2021, 2, 28),
+    location: "jamnagar",
+  },
+  {
+    id: "e4",
+    title: "Food",
+    amount: 650,
+    date: new Date(2021, 2, 28),
+    location: "rajkot",
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      title: " Car",
-      amount: "2500",
-      date: new Date(2021, 2, 28),
-      location: "baroda",
-    },
-    {
-      title: "Mobile",
-      amount: 230,
-      date: new Date(2021, 2, 28),
-      location: "surat",
-    },
-    {
-      title: "house EMI",
-      amount: 2500,
-      date: new Date(2021, 2, 28),
-      location: "jamnagar",
-    },
-    {
-      title: "Food",
-      amount: 650,
-      date: new Date(2021, 2, 28),
-      location: "rajkot",
-    },
-  ];
+  const [expenses, setExpenses] = useState(InitialExpense);
 
   const addExpenseHandler = (expense) => {
-    console.log("in app.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      console.log(prevExpenses);
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
